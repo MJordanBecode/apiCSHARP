2c67b7d0a15c05cc4d2adc9cdb694af28199c1ba
MasyJordan <masyjordan@gmail.com>
2025-07-02 11:38:09 +0200

    update

--- WebApplication1/Data/ContextDatabase.cs
+++ WebApplication1/Data/ContextDatabase.cs
@@ -1,9 +1,15 @@
+using DefaultNamespace;
 using Microsoft.EntityFrameworkCore;
 
-namespace DefaultNamespace
+namespace WebApplication1.Data
 {
     public class ContextDatabase : DbContext
        {
-           public ContextDatabase(DbContextOptions<ContextDatabase> options) : base(options) { }
+           public ContextDatabase(DbContextOptions<ContextDatabase> options) : base(options)
+           {
+               
+           }
+           public virtual DbSet<User> Users { get; set; }
+           public virtual DbSet<Address> Addresses { get; set; }
        } 
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,23 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 🔹 1. Lire la chaîne de connexion
+string? context = builder.Configuration.GetConnectionString("Connection");
 
+// 🔹 2. Ajouter le DbContext dans le conteneur DI
+builder.Services.AddDbContext<ContextDatabase>(options =>
+    options.UseSqlServer(context));
+
+// 🔹 3. Ajouter les autres services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 🔹 4. Configuration de la pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
